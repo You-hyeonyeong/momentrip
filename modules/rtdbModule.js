d07b929220074d21aa36a1b61c1c24908f07e517
You-hyeonyeong <dbgusdud412@naver.com>
2020-01-29 20:37:36 +0900

    기존 파이어베이스 모듈 삭제

--- modules/rtdbModule.js
+++
@@ -1,9 +0,0 @@
-
-var admin = require("firebase-admin");
-var serviceAccount = require("../config/halfmile-f9907-firebase-adminsdk-t5bbi-ded576e2ab.json");
-
-const firebaseDB = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = { firebaseDB };
